a3f2546bc3b56a420f2ce46095e9c50a105c650c
hdh853 <hdh853@gmail.com>
2025-09-01 21:37:23 +0900

    Update app.py

--- week00/app.py
+++ week00/app.py
@@ -20,7 +20,7 @@ def new_post():
         content = request.form.get('content')
 
         if not title or not content:
-            return "제목과 내용을 모두 입력해주세요.", 400
+            return "제목과 내용을 모두 입력해주세요jjjjj.", 400
 
         post = {
             'title': title,
@@ -45,3 +45,4 @@ def delete_post():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
